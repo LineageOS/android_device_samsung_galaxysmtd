8c8ea052f9e1c5e7b13856e5f716b04a0c499230
Sebastian Schmidt <yath@yath.de>
2011-09-21 14:01:11 +0200

    Enable camera shutter toggle
    Changes the libcameraservice.so put onto the device to the patched
    version and builds the toggleshutter program.
    
    Change I031461c9366f276a84c7eb72b629ceb45e8c1e92 has to be applied to
    aries-common to make this work; see there for how this works.
    
    After applying this commit you need to either re-run extract-files.sh or
    patch vendor/samsung/aries-common/aries-vendor-blobs.mk accordingly.
    
    Change-Id: I257fc8bc9200b078d7aa0a4a182dcf936786c26c

--- galaxysmtd.mk
+++ galaxysmtd.mk
@@ -104,6 +104,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	bdaddr_read
 
+# Shutter sound toggle
+PRODUCT_PACKAGES += \
+	toggleshutter
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
 	frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
