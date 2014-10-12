ae8a12dfa6f26d6f04c26156e6f46e3722bd896b
Oliver Middleton <olliemail27@gmail.com>
2014-10-12 22:45:04 +0100

    galaxysmtd: General cleanup and updates
     * Inherit full_base_telephony.mk instead of full.mk to avoid pulling in
    emulator files
     * Cleanup mk files
     * Add missing aries-keypad.kcm
     * Remove unused files
     * Fix file permissions
    
    Change-Id: I4548d88d18bd983236bf91161f5e93784de24cc7

--- cm.mk
+++ cm.mk
@@ -1,6 +1,3 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := GalaxyS
 
@@ -17,4 +14,8 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9000
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9000 TARGET_DEVICE=GT-I9000 BUILD_FINGERPRINT=samsung/GT-I9000/GT-I9000:2.3.5/GINGERBREAD/XXJVT:user/release-keys PRIVATE_BUILD_DESC="GT-I9000-user 2.3.5 GINGERBREAD XXJVT release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=GT-I9000 \
+    TARGET_DEVICE=GT-I9000 \
+    BUILD_FINGERPRINT=samsung/GT-I9000/GT-I9000:2.3.5/GINGERBREAD/XXJVT:user/release-keys \
+    PRIVATE_BUILD_DESC="GT-I9000-user 2.3.5 GINGERBREAD XXJVT release-keys"
